3ec8a47d7bcd97e4d3364c5710e720a22a52b9dc
Isabel Ramos <iramos1226@gmail.com>
2017-03-12 21:24:20 -0500

    completed assignment but could use improvement

--- food.js
+++ food.js
@@ -4,42 +4,63 @@ var foodString = "";
 
 function makeDom (data, dogCatData) {
 	var currentBrand;
-	var breedsPara = document.getElementsByClassName("breeds");
 
-	for (var i=0; i<data[dogCatData].length; i++) {
-		currentBrand = data[dogCatData][i];
-		foodString += `<div class="col-sm-4 col-md-2">`;
-		foodString += `<div class="panel panel-primary">`;
-		foodString += `<div class="panel-heading"`;
-		foodString += `<h3>${currentBrand.name}</h3>`;
-		foodString += `</div>`;
+	if (dogCatData === "dog_brands") {
+		for (var i=0; i<data[dogCatData].length; i++) {
+			currentBrand = data[dogCatData][i];
+			foodString += `<div class="col-sm-4 col-md-2">`;
+			foodString += `<div class="panel panel-primary">`;
+			foodString += `<div class="panel-heading"`;
+			foodString += `<h3>${currentBrand.name}</h3>`;
+			foodString += `</div>`;
 
-	if (breedsPara === undefined) {
-		breedsPara.style.display = "none";	
-	} else {
-		foodString += `<p class="breeds">${currentBrand.breeds}</p>`
-		}
+				for (var j=0; j<currentBrand.types.length; j++) {
+					var currentTypes = currentBrand.types[j];
+					foodString += `<p>${currentTypes.type}</p>`;
 
-			for (var j=0; j<currentBrand.types.length; j++) {
-				var currentTypes = currentBrand.types[j];
-				foodString += `<p>${currentTypes.type}</p>`;
-
-				for (var k=0; k<currentTypes.volumes.length; k++) {
-					var currentVolumes = currentTypes.volumes[k];
-					foodString += `<p>${currentVolumes.name} ${currentVolumes.price}</p>`;
-				
+					for (var k=0; k<currentTypes.volumes.length; k++) {
+						var currentVolumes = currentTypes.volumes[k];
+						foodString += `<p>${currentVolumes.name} ${currentVolumes.price}</p>`;
+					
+					}	
 				}	
-			}	
 
-			foodString += `</div></div>`;
+				foodString += `</div></div>`;
+		
+		foodInfoContainer.innerHTML = foodString;
+		}
+	} else {
+		for (var i=0; i<data[dogCatData].length; i++) {
+			currentBrand = data[dogCatData][i];
+			foodString += `<div class="col-sm-4 col-md-2">`;
+			foodString += `<div class="panel panel-primary">`;
+			foodString += `<div class="panel-heading"`;
+			foodString += `<h3>${currentBrand.name}</h3>`;
+			foodString += `</div>`;
+			foodString += `<p class="breeds">${currentBrand.breeds}</p>`;
 
-	foodInfoContainer.innerHTML = foodString;
+				for (var j=0; j<currentBrand.types.length; j++) {
+					var currentTypes = currentBrand.types[j];
+					foodString += `<p>${currentTypes.type}</p>`;
+
+					for (var k=0; k<currentTypes.volumes.length; k++) {
+						var currentVolumes = currentTypes.volumes[k];
+						foodString += `<p>${currentVolumes.name} ${currentVolumes.price}</p>`;
+					
+					}	
+				}	
+
+				foodString += `</div></div>`;
+		
+		foodInfoContainer.innerHTML = foodString;
+		}
 	}
 }
 
 
 function executeThisCodeAfterFileLoadDogCat () {
 	var data = JSON.parse(this.responseText);
+	// for (variable in object)
 	for (key in data) {
 		var dogCatData = key;
 		makeDom(data, dogCatData);	
@@ -62,7 +83,6 @@ myCatRequest.addEventListener("load", executeThisCodeAfterFileLoadDogCat);
 myCatRequest.addEventListener("error", executeThisCodeAfterFileFail);
 myCatRequest.open("GET", "cat-food.json");
 myCatRequest.send();
-
 
 
 
